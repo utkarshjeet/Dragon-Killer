d4cdf1e741e8a5126e1d7f8f960d155a941b2e21
utkarshjeet <utkarshyadav1029@gmail.com>
2026-02-20 16:53:38 +0530

    initial

---
+++ script.js
@@ -0,0 +1,19 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+
+let button1 = document.getElementById("btn-store");
+let button2 = document.getElementById("btn-cave");
+let button3 = document.getElementById("btn-dragon");
+
+
+
+
+
+
+
